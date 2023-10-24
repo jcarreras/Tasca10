d16d328fe448594a71a4b1db070424e15dd33e95
jcarreras <jcarreras@iesjoanramis.org>
2023-10-24 13:21:05 +0200

    ex10

---
+++ ex10.py
@@ -0,0 +1,14 @@
+#Definició de la funció
+def major(a):
+    if a>18:
+        print("És major d'edat")
+    elif a<18:
+        print("És menor d'edat")
+    else:
+        print("Té 18 anys")
+
+#Programa principal
+x = int(input("Introdueixi la seva edat: "))
+major(x)
+
+
